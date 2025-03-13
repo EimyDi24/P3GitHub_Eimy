7f0c1be80ed4613a071eb383c276cb40117325e7
fio-nose <fiorelh88@gmail.com>
2025-03-13 11:45:12 +0100

    Create CambioNuevaFuncionalidad.java
    Cambio a la nueva funcionalidad

---
+++ CambioNuevaFuncionalidad.java
@@ -0,0 +1,12 @@
+package miApp;
+
+public class CambioNuevaFuncionalidad {
+	public static void main(String[] args) {
+        System.out.println("Esta es una nueva funcionalidad.");
+        metodoCambio();
+    }
+	
+	public static void metodoCambio() {
+		System.out.println("Metodo nuevo.");
+	}
+}
